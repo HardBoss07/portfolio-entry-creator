9767cb45a6c28378d44303ebeedae89f385b59f2
HardBoss07 <matteo@bosshard.net>
2025-08-20 09:51:15 +0200

    base setup for further dev

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+use std::io::{BufReader, Write};
+use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(name = "portfolio-entry-creator", version, about = "Portfolio Entry Creator")]
+struct Args {
+    /// Input File
+    #[arg(short = 'i', long, value_name = "INPUT_FILE")]
+    input_file: PathBuf,
+
+    /// Output File
+    #[arg(short = 'o', long, value_name = "OUTPUT_FILE")]
+    output_file: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{} o, {} i", args.output_file.display(), args.input_file.display());
+}
